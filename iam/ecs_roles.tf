d6ccb1f2b0725e598d7f60d5cf1d9007fecb525e
Manisha Sah <manisha@Manishas-MacBook-Air.local>
2025-11-16 01:28:58 +0530

    adding first role

--- iam/ecs_roles.tf
+++ iam/ecs_roles.tf
@@ -1,6 +1,6 @@
 resource aws_iam_role ecs_task_execution_role {
   name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role_policy.json
+  assume_role_policy = aws_iam_policy_document.ecs_task_execution_assume_role_policy.json
 }
 resource aws_iam_policy_document ecs_task_execution_assume_role_policy {
   statement {
@@ -18,7 +18,7 @@ resource aws_iam_role_policy_attachment ecs_task_execution_role_policy_attachmen
 }
 resource aws_iam_role ecs_task_role {
   name               = "ecsTaskRole"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
+  assume_role_policy = aws_iam_policy_document.ecs_task_assume_role_policy.json
 }
 resource aws_iam_policy_document ecs_task_assume_role_policy {
     statement {
@@ -26,21 +26,6 @@ resource aws_iam_policy_document ecs_task_assume_role_policy {
     principals {
         type        = "Service"
         identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-    }
-}
-resource "aws_iam_role" "provisioner_role" {
-    count              = var.create_provisioner_role ? 1 : 0
-    name               = "${var.app_name}-${var.environment}-provisioner-role"
-    assume_role_policy = data.aws_iam_policy_document.provisioner_assume_role_policy.json
-}
-resource "aws_iam_policy_document" "provisioner_assume_role_policy" {
-    statement {
-    effect = "Allow"
-    principals {
-        type        = "Service"
-        identifiers = ["ec2.amazonaws.com", "ecs-tasks.amazonaws.com"]
     }
     actions = ["sts:AssumeRole"]
     }
