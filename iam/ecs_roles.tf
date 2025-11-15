24c0151bd7861fce9b6c899e8b45c07ac0da32d7
Manisha Sah <manisha@Manishas-MacBook-Air.local>
2025-11-16 01:42:32 +0530

    adding first role

--- iam/ecs_roles.tf
+++ iam/ecs_roles.tf
@@ -1,8 +1,4 @@
-resource aws_iam_role ecs_task_execution_role {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = aws_iam_policy_document.ecs_task_execution_assume_role_policy.json
-}
-resource aws_iam_policy_document ecs_task_execution_assume_role_policy {
+data "aws_iam_policy_document" "ecs_task_execution_assume_role_policy" {
   statement {
     effect = "Allow"
     principals {
@@ -12,21 +8,29 @@ resource aws_iam_policy_document ecs_task_execution_assume_role_policy {
     actions = ["sts:AssumeRole"]
   }
 }
-resource aws_iam_role_policy_attachment ecs_task_execution_role_policy_attachment {
-    role       = aws_iam_role.ecs_task_execution_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role_policy.json
 }
-resource aws_iam_role ecs_task_role {
-  name               = "ecsTaskRole"
-  assume_role_policy = aws_iam_policy_document.ecs_task_assume_role_policy.json
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-resource aws_iam_policy_document ecs_task_assume_role_policy {
-    statement {
+
+data "aws_iam_policy_document" "ecs_task_assume_role_policy" {
+  statement {
     effect = "Allow"
     principals {
-        type        = "Service"
-        identifiers = ["ecs-tasks.amazonaws.com"]
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
     actions = ["sts:AssumeRole"]
-    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "ecsTaskRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
 }
